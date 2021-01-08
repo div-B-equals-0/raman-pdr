833e90c8d9af62634c904fc8eaf3f0cf33e27da1
William Henney <will@henney.org>
2021-01-08 00:18:02 -0600

    Add to the two spin-off papers

--- extinct-paper/auto/orion-extinct.el
+++ extinct-paper/auto/orion-extinct.el
@@ -5,13 +5,13 @@
                      '(("aastex63" "twocolumn" "times")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("babel" "spanish" "es-minimal" "english") ("inputenc" "utf8") ("newtxmath" "varg")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -44,6 +44,7 @@
    (LaTeX-add-labels
     "sec:introduction"
     "fig:3color-extinction"
+    "sec:orion-s-region"
     "sec:other-meas-extinct"
     "sec:diff-interst-bands"
     "sec:forb-metal-doubl"
